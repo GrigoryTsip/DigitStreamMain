e33ed8dc55d11f75397ee3204736c7ffebd9de83
Grigory Tsiperman <113826725+GrigoryTsip@users.noreply.github.com>
2022-10-29 05:55:00 +0300

    Update Main.java
    Выложен правильный исходный текст

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        System.out.println("Исходный массив целых чисел: " + intList.toString());
+
+        List<Integer> positive = intList.stream()
+                .filter(x -> x > 0).toList();
+        System.out.println("Отфильтрованный массив целых положительных чисел: " + positive.toString());
+
+        Stream<Integer> even = positive.stream()
+                .filter(x -> x % 2 == 0)
+                .sorted(Comparator.naturalOrder());
+        Object[] o = even.toArray();
+        System.out.println("Отсортированный массив четных положительных чисел: " + Arrays.toString(o));
     }
 }
